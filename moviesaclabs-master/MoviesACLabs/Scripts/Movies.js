f8520d5c27a0b34cdb51cffefd93c4f2dba87c73
Roxi Ciobotea <roxi.ciobotea@yahoo.com>
2016-04-11 16:45:48 +0300

    End of lab

--- moviesaclabs-master/MoviesACLabs/Scripts/Movies.js
+++ moviesaclabs-master/MoviesACLabs/Scripts/Movies.js
@@ -1,1 +1,23 @@
-﻿
+﻿var cnt = 0;
+var movies=[];
+
+$("#addButton").on("click", function () {
+    var movieName = $("#movieName").val();
+    $("#moviesList").append('<li id=movie' + cnt++ + '>' + movieName + ' <button id="deleteButton">Delete</button></li>');
+    $("#movieName").val("").focus();
+    movies[cnt++] = movieName;
+});
+
+$("#movieName").on("keypress", function (event) {
+    if (event.which == 13) {
+        var movieName = $("#movieName").val();
+        $("#moviesList").append('<li>' + movieName + ' <button class="deleteButton">Delete</button></li>');
+        $("#movieName").val("").focus();
+        movies[cnt++] = movieName;
+    }
+});
+
+$("body").on("click", ".deleteButton", function (event) {
+    var toDelete = event.currentTarget.closest("li");
+    toDelete.remove();
+});
