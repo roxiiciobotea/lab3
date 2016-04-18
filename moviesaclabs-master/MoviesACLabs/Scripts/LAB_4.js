b1fbfbc6e2d626c4ad20e6af501a916810880df3
Roxi Ciobotea <roxi.ciobotea@yahoo.com>
2016-04-18 16:49:05 +0300

    Lab4

---
+++ moviesaclabs-master/MoviesACLabs/Scripts/LAB_4.js
@@ -0,0 +1,21 @@
+﻿var app = angular.module("main", []);
+
+app.controller('MainCtrl', function ($scope) {
+    $scope.allTasks=["Hardcoded Task"];
+    $scope.activeTasks = [];
+    $scope.doneTasks = [];
+    $scope.crtTask = '';
+    $scope.numberOfTasks = $scope.allTasks.length;
+
+    $scope.addTask = function () {
+        $scope.allTasks.push($scope.crtTask);
+        $scope.crtTask = "";
+        $scope.numberOfTasks = $scope.allTasks.length;
+    };
+
+    //not working
+    $scope.remove = function (index) {
+        $scope.allTasks.splice(index, 1);
+        $scope.numberOfTasks = $scope.allTasks.length;
+    };
+});
